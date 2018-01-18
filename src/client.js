abe95818ed90726dbb3d5f1f58730df353bd89f8
Justin Maher <justin.maher@logmein.com>
2018-01-18 10:31:13 -0800

    add comments to client

--- src/client.js
+++ src/client.js
@@ -1,10 +1,10 @@
 const WebSocket = require('ws');
 const { isValidMessage } = require('./common');
-process.stdin.setEncoding('utf8');
-
-console.log(`argv: ${process.argv}`);
 const from = process.argv[2] || 'unknown';
 const serverAddress = process.argv[3] || 'ws://localhost:8080';
+
+// read stdin as utf8 text
+process.stdin.setEncoding('utf8');
 
 console.log(`serverAddress: ${serverAddress}`);
 console.log(`from: ${from}`);
@@ -13,6 +13,7 @@ const ws = new WebSocket(serverAddress);
 
 ws.on('message', message => {
     const parsed = JSON.parse(message)
+    // only print valid messages
     if (isValidMessage(parsed)) {
         process.stdout.write(`${parsed.from}: ${parsed.content}`);
     } else {
@@ -20,4 +21,5 @@ ws.on('message', message => {
     }
 });
 
+// observe stdin for new meessages to send
 process.stdin.on('data', content => ws.send(JSON.stringify({ from, content })));
